b337914781914db782c44417d3ef2547288beaf4
surajyadav-CEO <59860772+surajyadav-CEO@users.noreply.github.com>
2020-04-07 13:41:18 +0530

    Create JavaPrimalityTest.java

---
+++ JavaPrimalityTest.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String n = scanner.nextLine();
+
+        scanner.close();
+        BigInteger integer=new BigInteger(n);
+        if(integer.isProbablePrime(1)){
+            System.out.println("Prime");
+        }
+        else
+        {
+            System.out.println("not prime");
+        }
+    }
+}
